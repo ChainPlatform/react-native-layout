e2415f7e70071f1f02ddeab2e0ad954f6d51d945
Chain Platform <support@chainplatform.net>
2025-11-04 14:10:53 +0700

    change cart

--- src/Cart.js
+++ src/Cart.js
@@ -5,12 +5,11 @@ export function getCartLayout(layoutWidth, isDesktop = true) {
     let totalItem = 1;
     if (layoutWidth > sdkStyles.base_desktop) {
         totalItem = 2;
-
     }
-    itemWidth = layoutWidth - (2 * (totalItem + 1) * basePadding);
-
+    let itemWidth = (layoutWidth - (2 * (totalItem + 1) * basePadding)) / totalItem;
     let view = {};
     view.basePadding = basePadding;
     view.itemWidth = itemWidth;
+    view.totalItem = totalItem;
     return view;
 }
