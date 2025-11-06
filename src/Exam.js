613fd9c4e366c9b52d93c63edc889728bc2a24a8
Chain Platform <support@chainplatform.net>
2025-11-06 21:05:19 +0700

    add exam

--- src/Exam.js
+++ src/Exam.js
@@ -1,18 +1,25 @@
-import sdkStyles from './styles';
+import sdkStyles, { setSize } from './styles';
 
 export function getDoExamLayout(layoutWidth) {
     let totalItem = 1;
+    let subSize = 1;
+    let mainSize = 1;
+    const basePadding = setSize(15);
     let mainViewWidth = layoutWidth;
     let subViewWitdh = mainViewWidth;
     if (layoutWidth > sdkStyles.base_desktop) {
         totalItem = 2;
-        mainViewWidth = layoutWidth * 0.7;
-        subViewWitdh = layoutWidth - mainViewWidth;
+        subSize = 0.3;
+        mainSize = 0.7;
     }
+    const baseWidth = layoutWidth - (2 * (totalItem + 1) * basePadding);
+    mainViewWidth = baseWidth * mainSize;
+    subViewWitdh = baseWidth * subSize;
 
     return {
         totalItem,
         mainViewWidth,
-        subViewWitdh
+        subViewWitdh,
+        basePadding
     };
 }
